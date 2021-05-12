08363c34ce8d2e20d5c35a63dda1ef480c4c4c0f
B22-User <82983412+gcotten12@users.noreply.github.com>
2021-05-11 20:56:51 -0500

    Added new classes App and MyApp for practice with custom classes

---
+++ src/day44_custom_classes/App.java
@@ -0,0 +1,10 @@
+package day44_custom_classes;
+
+public class App {
+    String name;
+    String version;
+
+    public void open() {
+        System.out.println("Launch " + name + " to current version : " + version);
+    }
+}
---
+++ src/day44_custom_classes/MyApp.java
@@ -0,0 +1,11 @@
+package day44_custom_classes;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+        App myApp = new App();
+        myApp.name = "Youtube";
+        myApp.version = "16.18.5";
+        myApp.open();
+    }
+}
