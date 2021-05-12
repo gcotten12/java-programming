c616fbe1e4494bfebf26c3c4d488f8e2aa72bf5f
B22-User <82983412+gcotten12@users.noreply.github.com>
2021-05-11 19:45:00 -0500

    created Animal class where we created objects and AnimalObjects class to print them

---
+++ src/day44_custom_classes/AnimalObjects.java
@@ -0,0 +1,10 @@
+package day44_custom_classes;
+
+public class AnimalObjects {
+
+    public static void main(String[] args) {
+        Animals animal = new Animals();
+        animal.eat();
+        animal.speak();
+    }
+}
---
+++ src/day44_custom_classes/Animals.java
@@ -0,0 +1,12 @@
+package day44_custom_classes;
+
+public class Animals {
+    String type = "Koala";
+    public void eat() {
+        System.out.println("the " + type + " is eating");
+    }
+    public void speak() {
+        System.out.println("the " + type + " is speaking to the other Koala's");
+    }
+}
+
