5f40435c19da279555f775f00021dae6313d1f6b
B22-User <82983412+gcotten12@users.noreply.github.com>
2021-05-11 20:49:21 -0500

    add more code

--- src/day44_custom_classes/AnimalObjects.java
+++ src/day44_custom_classes/AnimalObjects.java
@@ -4,7 +4,15 @@ public class AnimalObjects {
 
     public static void main(String[] args) {
         Animals animal = new Animals();
-        animal.eat();
+        System.out.println(animal.type);
+        animal.eat("grass");
         animal.speak();
+        // create object cheetahObj
+        Animals cheetaObj = new Animals();
+        cheetaObj.type = "cheeta";
+        System.out.println(cheetaObj.type);
+        cheetaObj.eat("antelope");
+        cheetaObj.speak();
+
     }
 }
--- src/day44_custom_classes/Animals.java
+++ src/day44_custom_classes/Animals.java
@@ -2,11 +2,17 @@ package day44_custom_classes;
 
 public class Animals {
     String type = "Koala";
-    public void eat() {
-        System.out.println("the " + type + " is eating");
+
+//    public void eat() {
+//        System.out.println("eating");
+//    }
+
+    public void eat(String food) {
+        System.out.println("eating " + food);
     }
+
     public void speak() {
-        System.out.println("the " + type + " is speaking to the other Koala's");
+        System.out.println("speaking");
     }
 }
 
