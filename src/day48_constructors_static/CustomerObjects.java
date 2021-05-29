b7ee112a296691aa6840b6f22a5e71f6bd22a92d
B22-User <82983412+gcotten12@users.noreply.github.com>
2021-05-29 10:27:06 -0500

    added cs1 with default values

--- src/day48_constructors_static/CustomerObjects.java
+++ src/day48_constructors_static/CustomerObjects.java
@@ -4,7 +4,8 @@ public class CustomerObjects {
 
     public static void main(String[] args) {
 
-
+        Customer cs1 = new Customer();
+        System.out.println(cs1); // printing with default values
 
     }
 
