416ad72ca087b3182bdf3ac0fcbec430b6b25f94
B22-User <82983412+gcotten12@users.noreply.github.com>
2021-05-29 10:28:49 -0500

    added more code to cs1

--- src/day48_constructors_static/CustomerObjects.java
+++ src/day48_constructors_static/CustomerObjects.java
@@ -6,6 +6,10 @@ public class CustomerObjects {
 
         Customer cs1 = new Customer();
         System.out.println(cs1); // printing with default values
+        cs1.setId(1);
+        cs1.setName("Bob");
+        System.out.println(cs1);
+
 
     }
 
