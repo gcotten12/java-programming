a5c408fb8ad35f1d6fc06b5f7857d072fbd85e32
B22-User <82983412+gcotten12@users.noreply.github.com>
2021-05-29 15:24:16 -0500

    added BusObjects class to implement composition method. Added code changes to classes

--- src/day48_constructors_static/Engine.java
+++ src/day48_constructors_static/Engine.java
@@ -3,6 +3,9 @@ package day48_constructors_static;
 public class Engine {
     private int cylinders;
 
+    public Engine() {
+    }
+
     public Engine(int cylinders) {
         this.cylinders = cylinders;
     }
