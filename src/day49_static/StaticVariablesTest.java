03312bab6574fbc9d303c3f7cd097a9eebf2da8f
B22-User <82983412+gcotten12@users.noreply.github.com>
2021-06-01 21:03:35 -0500

    added more code

---
+++ src/day49_static/StaticVariablesTest.java
@@ -0,0 +1,13 @@
+package day49_static;
+
+public class StaticVariablesTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("StaticVariables.name = " + StaticVariables.name);
+        System.out.println("StaticVariables.userName = " + StaticVariables.userName);
+        System.out.println("StaticVariables.age = " + StaticVariables.age);
+        System.out.println("StaticVariables.price = " + StaticVariables.price);
+
+    }
+}
