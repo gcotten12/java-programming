4e216c568189579a7f949320c4714f4d575347a8
B22-User <82983412+gcotten12@users.noreply.github.com>
2021-06-22 18:49:53 -0500

    added more code to Greetings

--- src/day56_abstraction/Greeting.java
+++ src/day56_abstraction/Greeting.java
@@ -27,5 +27,21 @@ class GreetingActions {
         Greeting gt = new MountainLanguage();
         gt.hi();
         gt.bye();
+        Greeting gt2 = new Japanese();
+        gt2.hi();
+        gt2.bye();
+    }
+}
+
+class Japanese implements Greeting {
+
+    @Override
+    public void hi() {
+        System.out.println("Kon'nichiwa");
+    }
+
+    @Override
+    public void bye() {
+        System.out.println("Sayōnara");
     }
 }
